f70fb38ba0c75cd5e9d6c36e47fd5214e3080966
Esther Yoo <liletchie@gmail.com>
2023-01-12 20:23:23 -0500

    adding react and webpack

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Portfolio
 
-This portfolio is not only to talk a little bit more about the projects I have workd on during my time in Fullstack GraceHopper, but it was also a way for me to create and deploy a website that used only vanilla JavaScript, HTML, and CSS.
+Portfolio of my my work in Fullstack Grace-Hopper.
 
 link: https://esther-yoo-portfolio.web.app/
--- public/script.js
+++ public/script.js
@@ -1,18 +1,18 @@
-function openCity(evt, project) {
-  let i, tabcontent, tablinks;
+// function openCity(evt, project) {
+//   let i, tabcontent, tablinks;
 
-  tabcontent = document.getElementsByClassName("tabContent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+//   tabcontent = document.getElementsByClassName("tabContent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
 
-  tablinks = document.getElementsByClassName("tabLinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+//   tablinks = document.getElementsByClassName("tabLinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
 
-  document.getElementById(project).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+//   document.getElementById(project).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
 
-document.getElementById("defaultOpen").click();
+// document.getElementById("defaultOpen").click();
